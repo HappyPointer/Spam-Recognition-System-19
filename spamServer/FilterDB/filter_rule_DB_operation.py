7e9ec82d5fee04cbb6393e41b53d8cf342161487
HappyPointer <43541458+HappyPointer@users.noreply.github.com>
2019-09-10 10:30:42 +0800

    Add files via upload

--- spamServer/FilterDB/filter_rule_DB_operation.py
+++ spamServer/FilterDB/filter_rule_DB_operation.py
@@ -1,9 +1,14 @@
+# 该 py 文件提供了操作数据库的相关接口函数
+# 作者：丁婧伊
+# 创建日期：2019-8-27
+# 最后修改日期：2019-9-5
+
 import pymysql
-import json
 
-
+# 该类是一个 Agent 类，通过创建 Filter_operation 对象，调用该对象的成员函数即可对数据库进行许可的操作
 class Filter_operation:
 
+    # 连接到已有数据库
     def conn(self, host = "127.0.0.1", user = "root", password = "Heyingzhi666@", database = "spamServer"):
         """
         连接数据库
@@ -16,7 +21,8 @@ class Filter_operation:
         db = pymysql.connect(host, user, password, database)
         return db
 
-
+    # 查询数据库中拥有的所有的用户自定义过滤规则
+    # 该函数在处理用户请求的线程中不会被调用，仅在服务器管理员希望查看数据库状态时被调用
     def find_all_owner(self):
         """
         查看表中所有的数据,返回一个json数据
@@ -46,14 +52,9 @@ class Filter_operation:
             print("查看所有数据时发生错误！", e)
         # 关闭数据库
         db.close()
-        # # 创造字典对象
-        # server_dict = {'action':'response-info', 'content': server_list}
-        # # 字典对象转换为json数据
-        # server_json = json.dumps(server_dict)
-        # # 返回json数据
-        # return server_json
         return server_list
 
+    # 向服务器中新加入一条用户自定义过滤规则
     def add_one_rule(self, client_json):
         """
         增加一条过滤规则，从客户端传入一条json数据(字符串形式),服务器返回一条json数据表示操作结果，1代表正确，-1代表不正确
@@ -91,6 +92,7 @@ class Filter_operation:
         # 返回json数据表示操作结果
         return server_dir
 
+    # 删除数据库中某一条已有用户自定义过滤规则
     def delete_one_rule(self, client_json):
         """
         删除一条规则，如果表中有多条相同的规则则全部都删去
@@ -122,11 +124,9 @@ class Filter_operation:
         db.close()
         # 创建字典对象，result=1操作成功，result=-1操作失败
         server_dir = {'action': 'response-post', 'content': result}
-        # 转换为json数据
-        # server_json = json.dumps(server_dir)
-        # 返回json数据表示操作结果
         return server_dir
 
+    # 查询某一个特定用户拥有的所有的自定义过滤规则
     def search_owner(self, client_json):
         """
         查找一个用户的所有过滤规则,客户端传递json数据，读取其中的owner进行查询，服务器返回一个json数据表示结果
@@ -164,11 +164,10 @@ class Filter_operation:
         db.close()
         # 创造字典对象
         server_dict = {'action': 'response-info', 'content': server_list}
-        # 字典对象转换为json数据
-        # server_json = json.dumps(server_dict)
-        # 返回json数据
         return server_dict
 
+    # 该函数将会将数据库完全清空
+    # 该函数在处理用户请求的线程中不会被调用，仅在服务器管理员觉得必要时清空数据库
     def clean_table(self):
         """
         清空表
