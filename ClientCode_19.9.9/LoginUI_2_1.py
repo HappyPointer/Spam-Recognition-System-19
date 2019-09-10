02f9614b4afde383ffd2855953ba890ff801a82c
ggccxj <48987607+ggccxj@users.noreply.github.com>
2019-09-10 11:18:16 +0800

    蔡夏菁_2019-9-10_11:17
    加注释

--- ClientCode_19.9.9/LoginUI_2_1.py
+++ ClientCode_19.9.9/LoginUI_2_1.py
@@ -1,10 +1,15 @@
+"""
+作者：蔡夏菁，郭振东，何颖智，丁婧伊
+描述：登录界面文件
+创建日期：2019-8-20
+最后修改日期：2019-9-10
+"""
+
 import pathlib
 import sys
 import traceback
 from Popup_Win import *
-from qtpy import QtCore
 from functools import partial
-
 from LoginAndGetMail2_6 import *
 import MainUI3_1
 from SettingOperations import *
@@ -16,7 +21,11 @@ import ctypes
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
 
 
-class RunThread(QtCore.QThread):
+class RunThread(QThread):
+    """
+    作者：郭振东
+    描述：主界面的子线程，在后台处理主界面所需数据并提供反馈
+    """
     # python3,pyqt5与之前的版本有些不一样
     #  通过类成员对象定义信号对象
     #在这里我新增了三个属性，以后开发会用到
@@ -28,6 +37,10 @@ class RunThread(QtCore.QThread):
     my_intensity=''
 
     def __init__(self, parent=None,serv=None,rulelist=[],mailadress=None,intensity=''):
+        """
+        作者：郭振东
+        描述：初始化该线程
+        """
         super(RunThread, self).__init__()
         self.my_serv=serv
         self.my_rulelist=rulelist
@@ -35,9 +48,17 @@ class RunThread(QtCore.QThread):
         self.my_intensity=intensity
 
     def __del__(self):
+        """
+        作者：郭振东
+        描述：用于线程操作
+        """
         self.wait()
 
     def run(self):
+        """
+        作者：郭振东
+        描述：子线程后台操作
+        """
         #设置初始列表长度为
         #根据该函数获取邮箱内邮件 并获取邮件对应的判断结果
         print(self.my_intensity)
@@ -55,15 +76,6 @@ class RunThread(QtCore.QThread):
             if email_list:
                 #若果发现邮件列表增长，那么将增长的部分发送至judgenewemail函数进行判断
                 response_result_list=judgeNewMail(self.my_intensity,email_list,self.my_rulelist)
-                # for i in email_list:
-                    # try:
-                    #     # win = PopupWin()
-                    #     print("win")
-                    #     win.show(type=i[1], subject=i[0][0][0], sender=i[0][0][1],
-                    #              body=i[0][1]).showAnimation()
-                    #     print("win.show")
-                    # except Exception as e:
-                    #     print(e)
                 self._signal1.emit(str(response_result_list))
                 # 将邮件主体和判断结果作为信号发送
                 self._signal.emit(str(response_result_list))
@@ -71,22 +83,27 @@ class RunThread(QtCore.QThread):
             else:
                 pass
 
-class DeleteButton(QPushButton):
-
-    def setrow(self, row):
-        self.row = row
-
-
 class LoginUI(QWidget):
+    """
+    作者：蔡夏菁
+    描述：创建登录界面
+    """
     # 登陆子线程使用状态变量
     logining_status = False
 
     def __init__(self):
-        # 初始化————init__
+        """
+        作者：蔡夏菁
+        描述：初始化登录界面
+        """
         super().__init__()
         self.initLoginUI()
 
     def initLoginUI(self):
+        """
+        作者：蔡夏菁
+        描述：创建登录界面控件并布局以及美化
+        """
         # 设置widget组件的大小(w,h)
         self.resize(500, 400)
         # 设置widget大小不可变
@@ -123,8 +140,6 @@ class LoginUI(QWidget):
         # 大小
         font2.setPointSize(15)
         font2.setWeight(75)
-
-
 
         # 背景颜色设置
         self.background1 = QLabel(self)
@@ -199,30 +214,30 @@ class LoginUI(QWidget):
         # show()方法在屏幕上显示出组件
         self.show()
 
-    # 点击鼠标触发函数
     def clickbtn1(self):
+        """
+        作者：蔡夏菁、郭振东、丁婧伊
+        描述：登录按钮槽函数，用于显示主界面并进行一系列线程操作
+        """
         # 打印出输入框的信息
         textboxValue1 = self.account.text()
         textboxValue2 = self.password.text()
         if textboxValue1 == '' or textboxValue2 == '':
             QMessageBox.question(self, "提示", '请正确输入邮箱账号及授权码', QMessageBox.Ok, QMessageBox.Ok)
         else:
-            self.login.setText('登陆中 ')
+            self.login.setText('登录中 ')
             self.login.repaint()
-            # 创建子线程用于改变按钮上的“登陆中”字样
+            # 创建子线程用于改变按钮上的“登录中”字样
             self.logining_status = True
             thread = LoginingThread()
             thread.start()
-
-            print('login[[[[[[[[[[[[[[[[[[[[[[[[')
             MessageReturn = logIn(textboxValue1, textboxValue2)
-            print(']]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]')
-            # 结束之前创建的用于改变“登陆中”字样的子线程，并将按钮改为“登陆”字样
+            # 结束之前创建的用于改变“登录中”字样的子线程，并将按钮改为“登录”字样
             self.logining_status = False
-            self.login.setText("登陆")
+            self.login.setText("登录")
 
             # 获取返回信息
-            QMessageBox.question(self, "提示", MessageReturn[0], QMessageBox.Ok, QMessageBox.Ok)
+            QMessageBox.question(self, "提示", "登录成功！", QMessageBox.Ok, QMessageBox.Ok)
             if MessageReturn[0]=='登陆成功！':
                 temp = Setting_Operations()
                 intensity = temp.getIntensity(MessageReturn[2])
@@ -230,29 +245,22 @@ class LoginUI(QWidget):
                 # 转到主界面
                 MainUI.show()
                 MainUI.tray.show()
-                print(555555555555555555555)
                 # 设置连接信号槽函数
                 MainUI.intensity_slider.valueChanged.connect(self.valueChange)
                 MainUI.intensity_slider.sliderMoved.connect(self.valueChange)
                 MainUI.setting_ok_button.clicked.connect(self.confirmChange)
-                print(66666666666666666666666)
                 MainUI.Filter_create_button.clicked.connect(self.Filter_create)
                 MainUI.ButtonGroup.buttonClicked.connect(self.BGclicked)
-                print(77777777777777777777)
                 MainUI.btnleft_Setting.clicked.connect(self.Reload_filter_message)
-                print(88888888888888888888)
-                # 初始化时从文件中读取用户的过滤强度
 
+                # 初始化时从文件中读取用户的过滤强度
                 # 2019/9/9 21:15 丁婧伊
                 # 读取本地该用户的个性装扮
                 themeName = ""
                 try:
                     themeFile = textboxValue1 + 'Theme.txt'
                     fo = open(themeFile, "r")
-                    # 是否要加参数？
                     themeName = fo.read()
-                    # debug
-                    print("read()不加参数的结果", themeName)
                     fo.close()
                 except Exception as e:
                     traceback.print_exc()
@@ -260,7 +268,6 @@ class LoginUI(QWidget):
                 # 判断themeFile是否存在
                 path = pathlib.Path(themeName)
                 fileExist = path.is_file()
-                print("文件名：", themeName)
                 if fileExist:
                     # 如果该文件存在,初始化用户存储的个性装扮
                     MainUI.palette = QPalette()
@@ -295,14 +302,16 @@ class LoginUI(QWidget):
                 else:
                     # 默认强度
                     MainUI.intensity_slider.setValue(50)
-                print(999999999999999999999)
                 self.setVisible(False)
-
         # 清空输入框信息
         self.account.setText('')
         self.password.setText('')
 
     def mousePressEvent(self, event):
+        """
+        作者：蔡夏菁
+        描述：重写鼠标点击事件，使无边框窗口可拖动
+        """
         if event.button() == Qt.LeftButton:
             self.m_flag = True
             self.m_Position = event.globalPos() - self.pos()  # 获取鼠标相对窗口的位置
@@ -310,27 +319,33 @@ class LoginUI(QWidget):
             self.setCursor(QCursor(Qt.OpenHandCursor))
 
     def mouseMoveEvent(self, QMouseEvent):
+        """
+        作者：蔡夏菁
+        描述：重写鼠标移动事件，使无边框窗口可拖动
+        """
         if Qt.LeftButton and self.m_flag:
             self.move(QMouseEvent.globalPos() - self.m_Position)  # 更改窗口位置
             QMouseEvent.accept()
 
     def mouseReleaseEvent(self, QMouseEvent):
+        """
+             作者：蔡夏菁
+             描述：重写鼠标释放事件，使无边框窗口可拖动
+             """
         self.m_flag = False
         self.setCursor(QCursor(Qt.ArrowCursor))
 
     # 创建线程
     def user_thread(self,serv,mailadress,MainUI,intensity):
-        # dic_list = [{'id': 1, 'owner': 'a1', 'sender': '', 'key_word': '才寻鲲', 'type': 'black'}]
-        dic={}
-        #向服务器请求过滤配置
-        print(11111)
+        """
+        作者：郭振东
+        描述：创建子线程用于显示右下角接收消息弹窗
+        """
         try:
             dic=send_client('request-info',mailadress)
         except:
             traceback.print_exc()
-            print('请求用户已有规则失败，网络错误！')
             dic={'content':[]}
-        print('2')
         #创建一个新的线程对象
         print(dic['content'])
         self.thread = RunThread(None,serv,dic['content'],mailadress,intensity)
@@ -342,6 +357,10 @@ class LoginUI(QWidget):
 
     # 创建过滤器事件
     def Filter_create(self):
+        """
+        作者：蔡夏菁、郭振东
+        描述：主界面创建过滤器按钮槽函数，用于创建过滤规则
+        """
         # 读取输入数据
         textboxValue1 = MainUI.addresser2_text.text()
         textboxValue2 = MainUI.word_detect_text.text()
@@ -362,28 +381,20 @@ class LoginUI(QWidget):
                 QMessageBox.information(MainUI, "提示", '操作失败', QMessageBox.Ok, QMessageBox.Ok)
                 traceback.print_exc()
                 pass
-
-
-        # MainUI.spam_mail_button.setCheckable(False)
-        # MainUI.spam_mail_button.setCheckable(True)
-        # MainUI.normal_mail_button.setCheckable(False)
-        # MainUI.normal_mail_button.setCheckable(True)
-        # MainUI.star_mail_button.setCheckable(False)
-        # MainUI.star_mail_button.setCheckable(True)
         MainUI.addresser2_text.setText("")
         MainUI.word_detect_text.setText("")
 
-        # 刷新已有过滤器信息操作
-
-
     def Reload_filter_message(self):
+        """
+        作者：郭振东、蔡夏菁、何颖智
+        描述：主界面左侧设置按钮槽函数之一，用于在设置界面显示以及重载过滤规则
+        """
         # 用于过滤规则的字体
         font3 = QFont()
         font3.setFamily('微软雅黑')
         font3.setPointSize(9)
 
         # 创建说明栏
-        print(456465464654654654654564)
         MainUI.widget_filter = QWidget()
         MainUI.layout_filter = QHBoxLayout()
         MainUI.filter_item = QListWidgetItem()
@@ -411,45 +422,30 @@ class LoginUI(QWidget):
         MainUI.argList = []
 
         for i in self.thread.my_rulelist:
-
             MainUI.filter_additem = QListWidgetItem()
             MainUI.filter_additem.setSizeHint(QSize(670, 80))
-
             MainUI.filter_message = QLabel(MainUI)
-            rule_str='包含：'
-            # rule_str=rule_str+i['key_word']+'，发件人为：'+i['sender']+'时，该邮件会被识别为：'+i['type']
-
-
             MainUI.filter_message.setFont(font3)
+
+            rule_str='包含：'
             elidfont = QFontMetrics(font3)
             str_1 = elidfont.elidedText(i['key_word'], Qt.ElideRight, 120)
             str_2 = elidfont.elidedText(i['sender'], Qt.ElideRight, 100)
             rule_str = rule_str + str_1 + '，发件人为：' + str_2 + '时，该邮件会被识别为：' + i['type']
 
-            # elidfont = QFontMetrics(font3)
-            # MainUI.filter_message.setText(elidfont.elidedText(rule_str, Qt.ElideRight, MainUI.filter_message.width() * 0.9))
             MainUI.filter_message.setText(rule_str)
             num += 1
             MainUI.operationbtn_list.append(QPushButton('删除', MainUI))
-            # opr = MainUI.operationbtn_list[num]
-            # opr.setrow(num)
+
             MainUI.operationbtn_list[num].setFixedSize(32, 25)
             MainUI.operationbtn_list[num].setStyleSheet("QPushButton{color:blue}"
                                      "QPushButton:hover{color:black}"
                                      "QPushButton{background-color:rgb(255,255,255,0)}"
                                      "QPushButton{border:none;}")
             MainUI.operationbtn_list[num].setCursor(QCursor(Qt.PointingHandCursor))
-            # MainUI.operation = QPushButton('删除', MainUI)
-            # MainUI.operation.setFixedSize(32, 25)
-            # MainUI.operation.setStyleSheet("QPushButton{color:blue}"
-            #                          "QPushButton:hover{color:black}"
-            #                          "QPushButton{background-color:rgb(255,255,255,0)}"
-            #                          "QPushButton{border:none;}")
-            # MainUI.operation.setCursor(QCursor(Qt.PointingHandCursor))
 
             MainUI.layout_filter1 = QHBoxLayout()
             MainUI.layout_filter1.addWidget(MainUI.filter_message)
-            # MainUI.layout_filter1.addWidget(MainUI.operation)
             MainUI.layout_filter1.addWidget(MainUI.operationbtn_list[num])
             MainUI.widget_filter1 = QWidget()
             MainUI.widget_filter1.setLayout(MainUI.layout_filter1)
@@ -457,21 +453,13 @@ class LoginUI(QWidget):
             MainUI.listWidget_filter.addItem(MainUI.filter_additem)
             MainUI.listWidget_filter.setItemWidget(MainUI.filter_additem, MainUI.widget_filter1)
             MainUI.operationbtn_list[num].clicked.connect(partial(self.filter_delete,num))
-        #     MainUI.argList.append(len(MainUI.argList))
-        #     # MainUI.operationbtn_list[num].clicked.connect(lambda: self.filter_delete(MainUI, opr.row))
-        #
-        # for num in MainUI.argList:
-        #     print('num value is : ' + str(num))
-        #     MainUI.operationbtn_list[num].clicked.connect(lambda index=num:  self.filter_delete(MainUI, index))
-
             MainUI.operationbtn_list[num].clicked.connect(self.Reload_filter_message)
 
-
-        # 删除ListWidget中的某一行
-
-    def filter_delete(self,n):
-        # 清空原有的过滤规则信息
-        # print(MainUI.listWidget_filter.currentRow())
+    def filter_delete(self, n):
+        """
+        作者：蔡夏菁，郭振东
+        描述：设置界面删除按钮槽函数，用于删除过滤规则
+        """
         rule_list=[]
         print(self.thread.my_rulelist)
         print(n)
@@ -486,8 +474,11 @@ class LoginUI(QWidget):
             pass
         MainUI.listWidget_filter.clear()
 
-    #创建单选按钮事件
     def BGclicked(self):
+        """
+        作者：蔡夏菁
+        描述：获取过滤器界面单选按钮被选中事件
+        """
         # 读取点击的按钮
         if MainUI.ButtonGroup.checkedId() == 1:
             self.info = 'black'
@@ -576,7 +567,7 @@ class LoginingThread(QThread):
         self.logining_counter += 1
         if self.logining_counter == 4:
             self.logining_counter = 0
-            gui.login.setText('登陆中 ')
+            gui.login.setText('登录中 ')
         else:
             gui.login.setText(gui.login.text() + ".")
         gui.login.repaint()
